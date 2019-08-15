67a5ea2ce7e796a9fd46becb367265558bb60561
Madelaine Brower <browermb@dukes.jmu.edu>
2019-08-15 13:59:20 -0400

    added tf broadcaster

--- sensor_main/scripts/tf_publisher.py
+++ sensor_main/scripts/tf_publisher.py
@@ -2,16 +2,17 @@
 
 import rospy
 
-class TFPublisher(object):
-
-    def __init__(self):
-        rospy.init_node('tf_publisher')
-        rospy.Subscriber('/ndt_pose', PoseStamped, self.ndt_pose_callback)
-        #some publisher
-
-    def ndt_post_callback(self):
-        pass
+def handle_ndt_pose(self):
+    br = tf.TransformBroadcaster()
+    """
+    br.sendTransform((msg.x, msg.y, 0),
+  12                      tf.transformations.quaternion_from_euler(0, 0, msg.theta),
+  13                      rospy.Time.now(),
+  14                      turtlename,
+  15                      "world")"""
 
 if __name__ == "__main__":
-    TFPublisher()
+    rospy.init_node('tf_publisher')
+    rospy.Subscriber('/ndt_pose', PoseStamped, self.handle_ndt_pose)
+    rospy.spin()
 
